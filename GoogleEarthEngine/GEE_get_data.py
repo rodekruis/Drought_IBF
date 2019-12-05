ec764ed773d8ce074a389bac5b08290863de424b
Margutti <jmargutti@redcross.nl>
2019-12-05 11:10:23 +0100

    update GEE

--- GoogleEarthEngine/GEE_get_data.py
+++ GoogleEarthEngine/GEE_get_data.py
@@ -1,14 +1,18 @@
 import ee
 from GEE_utils import extract_data_EE, fcdict_to_df
+import pandas as pd
 
 # initialize GEE
 ee.Initialize()
 
 # define dictionary of countries, with location of admin level shapefiles on GEE
+
 country_dict = {
     "KE": 'users/jacopomargutti/ken_admbnda_adm1',
     "UG": 'users/jacopomargutti/uga_admbnda_adm1'
 }
+
+output_folder = 'data/'
 
 # loop over countries
 for country_abb, admin_file in country_dict.items():
@@ -16,14 +20,13 @@ for country_abb, admin_file in country_dict.items():
     # get country admin level shapefile
     country = ee.FeatureCollection(admin_file)
     # year range
-    year_start, year_end = 1981, 2019
+    year_start, year_end = 2000, 2019
 
-    print("-----------------------------------------------------------------------------------------")
-    print("                                 NDVI mean                                               ")
-    print("-----------------------------------------------------------------------------------------")
+    # NDVI
+    name = 'ndvi'
 
     # Collect Features:
-    fc_ndvi_mean = extract_data_EE(im_col="NASA/GIMMS/3GV0",
+    fc_ndvi_mean = extract_data_EE(im_col="MODIS/006/MOD13A2",
                                    fe_col=country,
                                    min_year=year_start,
                                    max_year=year_end,
@@ -31,18 +34,17 @@ for country_abb, admin_file in country_dict.items():
                                    max_month=12,
                                    reducer_time=ee.Reducer.mean(),
                                    reducer_space=ee.Reducer.mean())
-
     # Turn feature collection dict to a single dataframe
     df_ndvi_mean = fcdict_to_df(year_start, fc_ndvi_mean)
-    df_ndvi_mean = df_ndvi_mean.rename(columns={'mean': 'ndvi_mean'})
-    df_ndvi_mean.to_csv(country_abb + "_ndvi_adm1.csv", sep=',', index=False)
+    df_ndvi_mean = df_ndvi_mean.rename(columns={'mean': 'ndvi'})
+    df_ndvi_mean.to_csv(output_folder + country_abb + "_"+name+"_adm1.csv", sep=',', index=False)
 
-    print("-----------------------------------------------------------------------------------------")
-    print("                                 CHIRPS sum (monthly cumulative)                         ")
-    print("-----------------------------------------------------------------------------------------")
+    # Global Monthly Precipitation Estimates
+    name = 'precipitation_estimate'
+    print('start', name, '####################################')
 
     # Collect Features:
-    fc_p_sum = extract_data_EE(im_col="UCSB-CHG/CHIRPS/DAILY",
+    fc_p_sum = extract_data_EE(im_col="TRMM/3B43V7",
                                fe_col=country,
                                min_year=year_start,
                                max_year=year_end,
@@ -50,15 +52,36 @@ for country_abb, admin_file in country_dict.items():
                                max_month=12,
                                reducer_time=ee.Reducer.sum(),
                                reducer_space=ee.Reducer.mean())
-
     # Turn feature collection dict to a single dataframe
     df_p_sum = fcdict_to_df(year_start, fc_p_sum)
-    df_p_sum = df_p_sum.rename(columns={'sum': 'precipitation'})
-    df_p_sum.to_csv(country_abb + "_p_adm1.csv", sep=',', index=False)
+    df_p_sum = df_p_sum.rename(columns={'mean': 'precipitation_estimate'})
+    df_p_sum.to_csv(output_folder + country_abb + "_"+name+"_adm1.csv", sep=',', index=False)
 
-    print("-----------------------------------------------------------------------------------------")
-    print("                                 Land Surface Temperature mean                           ")
-    print("-----------------------------------------------------------------------------------------")
+    # Global Precipitation Measurements
+    name = 'precipitation_measurements'
+    print('start', name, '####################################')
+
+    df_p_tot = pd.DataFrame()
+    for GPM_which in ['JAXA/GPM_L3/GSMaP/v6/reanalysis', 'JAXA/GPM_L3/GSMaP/v6/operational']:
+        # Collect Features:
+        fc_p_sum = extract_data_EE(im_col=GPM_which,
+                                   fe_col=country,
+                                   min_year=year_start,
+                                   max_year=year_end,
+                                   min_month=1,
+                                   max_month=12,
+                                   reducer_time=ee.Reducer.sum(),
+                                   reducer_space=ee.Reducer.mean())
+
+        # Turn feature collection dict to a single dataframe
+        df_p_sum = fcdict_to_df(year_start, fc_p_sum)
+        df_p_sum = df_p_sum.rename(columns={'mean': 'precipitation_measure'})
+        df_p_tot = df_p_tot.append(df_p_sum, ignore_index=True)
+    df_p_tot.to_csv(output_folder + country_abb + "_"+name+"_adm1.csv", sep=',', index=False)
+
+    # Land Surface Temperature
+    name = 'land_surface_temperature'
+    print('start', name, '####################################')
 
     # Collect Features:
     fc_lst_mean = extract_data_EE(im_col="MODIS/006/MOD11A1",
@@ -72,15 +95,15 @@ for country_abb, admin_file in country_dict.items():
 
     # Turn feature collection dict to a single dataframe
     df_lst_mean = fcdict_to_df(year_start, fc_lst_mean)
-    df_lst_mean = df_lst_mean.rename(columns={'mean': 'lst_mean'})
-    df_lst_mean.to_csv(country_abb + "_lst_adm1.csv", sep=',', index=False)
+    df_lst_mean = df_lst_mean.rename(columns={'mean': 'lst'})
+    df_lst_mean.to_csv(output_folder + country_abb + "_"+name+"_adm1.csv", sep=',', index=False)
 
-    print("-----------------------------------------------------------------------------------------")
-    print("                                 soil moisture mean                                      ")
-    print("-----------------------------------------------------------------------------------------")
+    # Soil Moisture
+    name = 'soil_moisture'
+    print('start', name, '####################################')
 
     # Collect Features:
-    fc_soilmois_mean = extract_data_EE(im_col="NASA_USDA/HSL/soil_moisture",
+    fc_soilmois_mean = extract_data_EE(im_col="NASA/FLDAS/NOAH01/C/GL/M/V001",
                                        fe_col=country,
                                        min_year=year_start,
                                        max_year=year_end,
@@ -93,10 +116,4 @@ for country_abb, admin_file in country_dict.items():
     df_soilmois_mean = fcdict_to_df(year_start, fc_soilmois_mean)
     df_soilmois_mean = df_soilmois_mean.rename(columns={'mean': 'soilmois_mean'})
     # save NDVI dataframe to .CSV
-    df_soilmois_mean.to_csv(country_abb + "_sm_adm1.csv", sep=',', index=False)
-
-
-
-
-
-
+    df_soilmois_mean.to_csv(output_folder + country_abb + "_"+name+"_adm1.csv", sep=',', index=False)
